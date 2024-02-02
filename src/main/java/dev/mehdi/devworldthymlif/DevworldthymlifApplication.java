62b0da15a5900d08a31a79520772ebc9e63062fe
Mehdi Qaos <mehdi.qaos@gmail.com>
2024-02-02 16:09:43 +0100

    thymeleaf

---
+++ src/main/java/dev/mehdi/devworldthymlif/DevworldthymlifApplication.java
@@ -0,0 +1,13 @@
+package dev.mehdi.devworldthymlif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevworldthymlifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevworldthymlifApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mehdi/devworldthymlif/DevworldthymlifApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mehdi.devworldthymlif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevworldthymlifApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
